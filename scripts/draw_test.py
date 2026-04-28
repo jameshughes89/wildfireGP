d071be07c788231ba2177432037f6b7e29720158
JamesHughes89 <JamesHughes89@Gmail.com>
2026-04-28 13:44:57 -0300

    added draw manual test stub

---
+++ scripts/draw_test.py
@@ -0,0 +1,21 @@
+import matplotlib.pyplot as plt
+
+from wildfireGP.network import NodeState, create_grid
+from wildfireGP.render import draw
+
+graph = create_grid(50, 50, water_fraction=0.08, rock_fraction=0.08, seed=42)
+
+# Manually set a few nodes to non-UNBURNED states so all colours are visible.
+nodes = list(graph.nodes)
+for n in nodes[100:115]:
+    graph.nodes[n]["state"] = NodeState.BURNING
+for n in nodes[115:130]:
+    graph.nodes[n]["state"] = NodeState.BURNED
+for n in nodes[130:145]:
+    graph.nodes[n]["state"] = NodeState.TREATED
+
+fig, ax = plt.subplots(figsize=(8, 8))
+draw(graph, ax=ax)
+ax.set_title("draw() manual test")
+plt.tight_layout()
+plt.show()
